ad9ce9df46142a638b5742596d6caf1a42d1d328
David Hewitt <hewitt.20@gmail.com>
2014-11-05 14:15:00 +0000

    Update app.js
    removed banner

--- resources/scripts/javascript/app.js
+++ resources/scripts/javascript/app.js
@@ -1,15 +1,3 @@
-/*
- *    jQuery Prefinator v2.5
- *
- *	Description: The Prefintator is a Plugin specificly
- *	designed for kinetic solutions to enable students
- *	to select room preferences.
- *
- *	Author: David Hewitt
- *
- *	Website: www.kinetic-solutions.co.uk
- *
- */
 ;(function($, window, document, undefined) {
     var settings, functions = {
         init: function (){
